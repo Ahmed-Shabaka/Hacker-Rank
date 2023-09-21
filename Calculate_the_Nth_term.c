a3b95e08a0ee7e24dc95300a161083cdba0dd73d
Ahmed-Shabaka <118419357+Ahmed-Shabaka@users.noreply.github.com>
2023-09-22 00:31:01 +0300

    Update Calculate_the_Nth_term.c

--- Calculate_the_Nth_term.c
+++ Calculate_the_Nth_term.c
@@ -5,24 +5,31 @@
 //Complete the following function.
 
 
-int find_nth_term(int n, int a, int b, int c) {
-    if (n == 1) return a;
-    if (n == 2) return b;
-    if (n == 3) return c;
+int find_nth_term(int n, int a, int b, int c) 
+{
+    int temp;
+    static int counter=3;
     
-    return find_nth_term(n - 1, b, c, a + b + c);
+    if(1==n) return a;
+    if(2==n) return b;
+    if(3==n) return c;
+    
+    temp=a+b+c;
+    counter++;
+    
+    if(n!=counter) temp=find_nth_term(n, b, c, a+b+c);
+   
+    return temp;
 }
 
 int main() {
     int n, a, b, c;
+  
     scanf("%d %d %d %d", &n, &a, &b, &c);
-    
     int ans = find_nth_term(n, a, b, c);
-    printf("%d", ans);
     
+    printf("%d", ans); 
     return 0;
 }
-
-
 
 
