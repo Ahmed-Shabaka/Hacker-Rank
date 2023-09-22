23db26a5b872754a38e5765513c524f9b651e465
Ahmed-Shabaka <118419357+Ahmed-Shabaka@users.noreply.github.com>
2023-09-22 14:48:47 +0300

    Add files via upload

---
+++ Students_Marks_Sum.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+//Complete the following function.
+
+int marks_summation(int* marks, int number_of_students, char gender) 
+{
+    int counter=0;
+    int sum=0;
+    for(counter=0;counter<number_of_students;counter++)
+    {
+       if ('b' == gender) (counter%2 == 0)?(sum+=marks[counter]):(sum);
+      else (counter%2 != 0)?(sum+=marks[counter]):(sum);
+    }
+    
+    return sum;
+}
+
+int main() {
+    int number_of_students;
+    char gender;
+    int sum;
+  
+    scanf("%d", &number_of_students);
+    int *marks = (int *) malloc(number_of_students * sizeof (int));
+ 
+    for (int student = 0; student < number_of_students; student++) {
+        scanf("%d", (marks + student));
+    }
+    
+    scanf(" %c", &gender);
+    sum = marks_summation(marks, number_of_students, gender);
+    printf("%d", sum);
+    free(marks);
+ 
+    return 0;
+}
